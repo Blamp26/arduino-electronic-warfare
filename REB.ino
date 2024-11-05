f88bbf9f1c7f927a731151c386976f09f54ce675
Blamp26 <98893587+Blamp26@users.noreply.github.com>
2024-11-05 18:14:49 +0200

    REB.ino

--- REB.ino
+++ REB.ino
@@ -2,91 +2,102 @@
 #include <RF24.h>
 #include <nRF24L01.h>
 
-// Пины для nRF24L01
-#define CE_PIN 22  // Замените на нужный пин для ESP32
-#define CSN_PIN 21 // Замените на нужный пин для ESP32
+// Піни для підключення модуля nRF24L01 до ESP32
+// CE_PIN - використовується для активації радіомодуля
+#define CE_PIN 22  // Підключіть CE до GPIO 22 на ESP32
+// CSN_PIN - використовується для вибору модуля nRF24L01 під час обміну даними по SPI
+#define CSN_PIN 21 // Підключіть CSN до GPIO 21 на ESP32
 
+// Ініціалізація модуля RF24 з вказаними пинами CE та CSN
 RF24 radio(CE_PIN, CSN_PIN);
 
-#define CHANNELS 64
-int channel[CHANNELS];
-int line;
-char grey[] = " .:-=+*aRW";
+#define CHANNELS 64  // Кількість каналів для сканування
+int channel[CHANNELS]; // Масив для збереження даних про активність на каналах
+int line;              // Лічильник ліній для виводу результатів у консоль
+char grey[] = " .:-=+*aRW"; // Символи для відображення рівнів сигналу
 
+// Регістри модуля nRF24L01 для налаштування параметрів
 #define _NRF24_CONFIG 0x00
 #define _NRF24_EN_AA 0x01
 #define _NRF24_RF_CH 0x05
 #define _NRF24_RF_SETUP 0x06
 #define _NRF24_RPD 0x09
 
-const uint8_t num_channels = 64;
-int values[num_channels];
-int channels = 0;
-const byte address[6] = "00001";
-const int num_reps = 50;
-bool jamming = true;
+const uint8_t num_channels = 64; // Кількість каналів, що сканується
+int values[num_channels];       // Масив для збереження результатів сканування
+int channels = 0;               // Поточний канал
+const byte address[6] = "00001"; // Адреса для передачі даних
+const int num_reps = 50;        // Кількість повторень
+bool jamming = true;            // Прапорець для активації режиму глушіння
 
+// Функція для читання значення регістру модуля nRF24L01
 byte getRegister(byte r)
 {
   byte c;
-  digitalWrite(CSN_PIN, LOW);
-  c = SPI.transfer(r & 0x1F);
-  c = SPI.transfer(0);
-  digitalWrite(CSN_PIN, HIGH);
+  digitalWrite(CSN_PIN, LOW);          // Активуємо CSN для SPI-зв'язку
+  c = SPI.transfer(r & 0x1F);          // Передаємо адресу регістру
+  c = SPI.transfer(0);                 // Читаємо дані з регістру
+  digitalWrite(CSN_PIN, HIGH);         // Відключаємо CSN
   return c;
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  radio.begin();
-  radio.startListening();
-  radio.stopListening();
+  Serial.begin(115200);               // Ініціалізація серійного зв’язку
+  radio.begin();                      // Ініціалізація модуля RF24
+  radio.startListening();             // Переходимо в режим прийому
+  radio.stopListening();              // Завершуємо режим прийому
 
-  SPI.begin();
-  pinMode(CE_PIN, OUTPUT);
-  pinMode(CSN_PIN, OUTPUT);
-  disable();
+  SPI.begin();                        // Ініціалізація SPI
+  pinMode(CE_PIN, OUTPUT);            // Налаштування CE як вихід
+  pinMode(CSN_PIN, OUTPUT);           // Налаштування CSN як вихід
+  disable();                          // Деактивація модуля
 
-  powerUp();
-  setRegister(_NRF24_EN_AA, 0x0);
-  setRegister(_NRF24_RF_SETUP, 0x0F);
+  powerUp();                          // Активація модуля
+  setRegister(_NRF24_EN_AA, 0x0);     // Вимкнення автоматичного підтвердження (ACK)
+  setRegister(_NRF24_RF_SETUP, 0x0F); // Встановлення максимальної потужності передавача
 
-  Serial.println("Starting 2.4GHz Scanner...");
+  Serial.println("Запуск сканера на 2.4ГГц...");
   Serial.println();
-  Serial.println("Channel Layout");
-  printChannels();
+  Serial.println("Відображення каналів");
+  printChannels();                     // Вивід каналів у консоль
 }
 
+// Функція для встановлення значення в регістр модуля nRF24L01
 void setRegister(byte r, byte v)
 {
-  digitalWrite(CSN_PIN, LOW);
-  SPI.transfer((r & 0x1F) | 0x20);
-  SPI.transfer(v);
-  digitalWrite(CSN_PIN, HIGH);
+  digitalWrite(CSN_PIN, LOW);          // Активуємо CSN для SPI-зв'язку
+  SPI.transfer((r & 0x1F) | 0x20);     // Передаємо адресу регістру для запису
+  SPI.transfer(v);                     // Записуємо значення в регістр
+  digitalWrite(CSN_PIN, HIGH);         // Відключаємо CSN
 }
 
+// Вмикаємо живлення модуля
 void powerUp(void)
 {
   setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x02);
-  delayMicroseconds(130);
+  delayMicroseconds(130);              // Затримка для стабілізації
 }
 
+// Вимикаємо живлення модуля
 void powerDown(void)
 {
   setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) & ~0x02);
 }
 
+// Активація модуля
 void enable(void)
 {
   digitalWrite(CE_PIN, HIGH);
 }
 
+// Деактивація модуля
 void disable(void)
 {
   digitalWrite(CE_PIN, LOW);
 }
 
+// Перемикання модуля в режим прийому
 void setRX(void)
 {
   setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x01);
@@ -94,6 +105,7 @@ void setRX(void)
   delayMicroseconds(100);
 }
 
+// Сканування каналів для пошуку активності
 void scanChannels(void)
 {
   disable();
@@ -101,20 +113,21 @@ void scanChannels(void)
   {
     for (int i = 0; i < channels; i++)
     {
-      setRegister(_NRF24_RF_CH, (128 * i) / channels);
-      setRX();
-      delayMicroseconds(40);
-      disable();
-      if (getRegister(_NRF24_RPD) > 0) channel[i]++;
+      setRegister(_NRF24_RF_CH, (128 * i) / channels); // Встановлюємо канал
+      setRX();                                         // Перемикаємося в режим прийому
+      delayMicroseconds(40);                           // Затримка
+      disable();                                       // Деактивація
+      if (getRegister(_NRF24_RPD) > 0) channel[i]++;   // Перевірка наявності сигналу
     }
   }
 }
 
+// Функція для виведення активності на кожному каналі
 void outputChannels(void)
 {
   int norm = 0;
   for (int i = 0; i < CHANNELS; i++)
-    if (channel[i] > norm) norm = channel[i];
+    if (channel[i] > norm) norm = channel[i]; // Визначаємо максимальне значення
 
   Serial.print('|');
   for (int i = 0; i < CHANNELS; i++)
@@ -131,11 +144,13 @@ void outputChannels(void)
   Serial.println(norm);
 }
 
+// Функція для виведення інформації про канали
 void printChannels(void)
 {
   Serial.println(">      1 2  3 4  5  6 7 8  9 10 11 12 13  14                     <");
 }
 
+// Режим глушіння, який передає завадовий сигнал
 void jammer()
 {
   const char text[] = {random(0, 256), random(0, 256), random(0, 256), random(0, 256)};
@@ -153,28 +168,29 @@ void jammer()
   }
 }
 
+// Головний цикл програми
 void loop()
 {
-  Serial.print("Scanning channel: ");
+  Serial.print("Сканування каналу: ");
   Serial.println(channels + 1);
   if (jamming)
   {
-    Serial.print("JAMMING CHANNEL ");
+    Serial.print("ГЛУШЕННЯ КАНАЛУ ");
     Serial.println(channels + 1);
-    radio.setPALevel(RF24_PA_HIGH);
-    radio.setDataRate(RF24_2MBPS);
+    radio.setPALevel(RF24_PA_HIGH); // Встановлюємо високу потужність сигналу
+    radio.setDataRate(RF24_2MBPS);  // Встановлюємо швидкість передачі 2 Мбіт/с
   }
   while (jamming)
   {
-    jammer();
+    jammer();                       // Активуємо глушіння
   }
 
-  scanChannels();
-  outputChannels();
+  scanChannels();                   // Виконуємо сканування каналів
+  outputChannels();                 // Виводимо результат сканування
 
   if (line++ > 12)
   {
-    printChannels();
+    printChannels();                // Виводимо назви каналів
     line = 0;
   }
 }
