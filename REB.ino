0e03b95882a6df24e9b5ffde33a426092c92c02e
Blamp26 <98893587+Blamp26@users.noreply.github.com>
2024-11-05 18:07:52 +0200

    REB.ino

--- REB.ino
+++ REB.ino
@@ -1,121 +1,189 @@
-#define TX_PIN 23
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
 
-int minHighDuration = 50;  
-int maxHighDuration = 400; 
-int minLowDuration = 50;   
-int maxLowDuration = 400;  
+// Піни для підключення модуля nRF24L01 до ESP32
+// CE_PIN - використовується для активації радіомодуля
+#define CE_PIN 22  // Підключіть CE до GPIO 22 на ESP32
+// CSN_PIN - використовується для вибору модуля nRF24L01 під час обміну даними по SPI
+#define CSN_PIN 21 // Підключіть CSN до GPIO 21 на ESP32
 
-int getRandomDelay(int minDelay, int maxDelay) {
-  return random(minDelay, maxDelay);
+// Ініціалізація модуля RF24 з вказаними пинами CE та CSN
+RF24 radio(CE_PIN, CSN_PIN);
+
+#define CHANNELS 64  // Кількість каналів для сканування
+int channel[CHANNELS]; // Масив для збереження даних про активність на каналах
+int line;              // Лічильник ліній для виводу результатів у консоль
+char grey[] = " .:-=+*aRW"; // Символи для відображення рівнів сигналу
+
+// Регістри модуля nRF24L01 для налаштування параметрів
+#define _NRF24_CONFIG 0x00
+#define _NRF24_EN_AA 0x01
+#define _NRF24_RF_CH 0x05
+#define _NRF24_RF_SETUP 0x06
+#define _NRF24_RPD 0x09
+
+const uint8_t num_channels = 64; // Кількість каналів, що сканується
+int values[num_channels];       // Масив для збереження результатів сканування
+int channels = 0;               // Поточний канал
+const byte address[6] = "00001"; // Адреса для передачі даних
+const int num_reps = 50;        // Кількість повторень
+bool jamming = true;            // Прапорець для активації режиму глушіння
+
+// Функція для читання значення регістру модуля nRF24L01
+byte getRegister(byte r)
+{
+  byte c;
+  digitalWrite(CSN_PIN, LOW);          // Активуємо CSN для SPI-зв'язку
+  c = SPI.transfer(r & 0x1F);          // Передаємо адресу регістру
+  c = SPI.transfer(0);                 // Читаємо дані з регістру
+  digitalWrite(CSN_PIN, HIGH);         // Відключаємо CSN
+  return c;
 }
 
-void amplitudeModulation(int cycles) {
-  Serial.println("=== Початок амплітудної модуляції (AM) ===");
-  Serial.print("Кількість циклів модуляції: ");
-  Serial.println(cycles);
-  
-  for (int i = 0; i < cycles; i++) {
-    int highDuration = getRandomDelay(minHighDuration, maxHighDuration);
-    int lowDuration = getRandomDelay(minLowDuration, maxLowDuration);
+void setup()
+{
+  Serial.begin(115200);               // Ініціалізація серійного зв’язку
+  radio.begin();                      // Ініціалізація модуля RF24
+  radio.startListening();             // Переходимо в режим прийому
+  radio.stopListening();              // Завершуємо режим прийому
 
-    Serial.print("Цикл ");
-    Serial.print(i + 1);
-    Serial.print(": HIGH на ");
-    Serial.print(highDuration);
-    Serial.print(" мкс, LOW на ");
-    Serial.print(lowDuration);
-    Serial.println(" мкс");
+  SPI.begin();                        // Ініціалізація SPI
+  pinMode(CE_PIN, OUTPUT);            // Налаштування CE як вихід
+  pinMode(CSN_PIN, OUTPUT);           // Налаштування CSN як вихід
+  disable();                          // Деактивація модуля
 
-    analogWrite(TX_PIN, 255);
-    delayMicroseconds(highDuration);
-    analogWrite(TX_PIN, 127);
-    delayMicroseconds(lowDuration);
-  }
-  Serial.println("=== Кінець амплітудної модуляції (AM) ===");
+  powerUp();                          // Активація модуля
+  setRegister(_NRF24_EN_AA, 0x0);     // Вимкнення автоматичного підтвердження (ACK)
+  setRegister(_NRF24_RF_SETUP, 0x0F); // Встановлення максимальної потужності передавача
+
+  Serial.println("Запуск сканера на 2.4ГГц...");
+  Serial.println();
+  Serial.println("Відображення каналів");
+  printChannels();                     // Вивід каналів у консоль
 }
 
-void generateAdvancedNoise() {
-  int noiseDuration = random(3000, 7000); 
-  Serial.println("=== Генерація складних патернів перешкод ===");
-  Serial.print("Тривалість перешкод: ");
-  Serial.print(noiseDuration);
-  Serial.println(" мс");
-  
-  for (int i = 0; i < noiseDuration; i++) {
-    int highDuration = random(minHighDuration, maxHighDuration);
-    int lowDuration = random(minLowDuration, maxLowDuration);
+// Функція для встановлення значення в регістр модуля nRF24L01
+void setRegister(byte r, byte v)
+{
+  digitalWrite(CSN_PIN, LOW);          // Активуємо CSN для SPI-зв'язку
+  SPI.transfer((r & 0x1F) | 0x20);     // Передаємо адресу регістру для запису
+  SPI.transfer(v);                     // Записуємо значення в регістр
+  digitalWrite(CSN_PIN, HIGH);         // Відключаємо CSN
+}
 
-    Serial.print("Цикл ");
-    Serial.print(i + 1);
-    Serial.print(": HIGH на ");
-    Serial.print(highDuration);
-    Serial.print(" мкс, LOW на ");
-    Serial.print(lowDuration);
-    Serial.println(" мкс");
+// Вмикаємо живлення модуля
+void powerUp(void)
+{
+  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x02);
+  delayMicroseconds(130);              // Затримка для стабілізації
+}
 
-    digitalWrite(TX_PIN, HIGH);
-    delayMicroseconds(highDuration);
+// Вимикаємо живлення модуля
+void powerDown(void)
+{
+  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) & ~0x02);
+}
 
-    digitalWrite(TX_PIN, LOW);
-    delayMicroseconds(lowDuration);
+// Активація модуля
+void enable(void)
+{
+  digitalWrite(CE_PIN, HIGH);
+}
 
-    if (i % 200 == 0) {
-      minHighDuration = random(30, 100);
-      maxHighDuration = random(300, 600);
-      minLowDuration = random(30, 100);
-      maxLowDuration = random(300, 600);
-      Serial.println("Зміна параметрів тривалості HIGH/LOW");
+// Деактивація модуля
+void disable(void)
+{
+  digitalWrite(CE_PIN, LOW);
+}
+
+// Перемикання модуля в режим прийому
+void setRX(void)
+{
+  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x01);
+  enable();
+  delayMicroseconds(100);
+}
+
+// Сканування каналів для пошуку активності
+void scanChannels(void)
+{
+  disable();
+  for (int j = 0; j < 255; j++)
+  {
+    for (int i = 0; i < channels; i++)
+    {
+      setRegister(_NRF24_RF_CH, (128 * i) / channels); // Встановлюємо канал
+      setRX();                                         // Перемикаємося в режим прийому
+      delayMicroseconds(40);                           // Затримка
+      disable();                                       // Деактивація
+      if (getRegister(_NRF24_RPD) > 0) channel[i]++;   // Перевірка наявності сигналу
     }
   }
-  Serial.println("=== Кінець генерації перешкод ===");
 }
 
-void advancedSignalModulation() {
-  int modulationCycles = random(5, 15); 
-  Serial.println("=== Початок складної модуляції сигналу (AM) ===");
-  Serial.print("Кількість циклів модуляції: ");
-  Serial.println(modulationCycles);
-  
-  for (int i = 0; i < modulationCycles; i++) {
-    Serial.print("Амплітудна модуляція, цикл ");
-    Serial.println(i + 1);
-    
-    amplitudeModulation(random(500, 1000));
+// Функція для виведення активності на кожному каналі
+void outputChannels(void)
+{
+  int norm = 0;
+  for (int i = 0; i < CHANNELS; i++)
+    if (channel[i] > norm) norm = channel[i]; // Визначаємо максимальне значення
 
-    int delayDuration = random(500, 1000);
-    Serial.print("Пауза між серіями: ");
-    Serial.print(delayDuration);
-    Serial.println(" мс");
-    
-    delay(delayDuration);
+  Serial.print('|');
+  for (int i = 0; i < CHANNELS; i++)
+  {
+    int pos;
+    if (norm != 0) pos = (channel[i] * 10) / norm;
+    else pos = 0;
+    if (pos == 0 && channel[i] > 0) pos++;
+    if (pos > 9) pos = 9;
+    Serial.print(grey[pos]);
+    channel[i] = 0;
   }
-  Serial.println("=== Кінець складної модуляції сигналу (AM) ===");
+  Serial.print("| ");
+  Serial.println(norm);
 }
 
-void setup() {
-  pinMode(TX_PIN, OUTPUT);
-  randomSeed(analogRead(0));
-  
-  Serial.begin(115200);
-  Serial.println("=== Ініціалізація завершена ===");
+// Функція для виведення інформації про канали
+void printChannels(void)
+{
+  Serial.println(">      1 2  3 4  5  6 7 8  9 10 11 12 13  14                     <");
 }
 
-void loop() {
-  Serial.println("=== Новий цикл роботи ===");
-  
-  generateAdvancedNoise();
-  int noisePause = random(1000, 3000); 
-  Serial.print("Пауза перед наступною серією перешкод: ");
-  Serial.print(noisePause);
-  Serial.println(" мс");
-  
-  delay(noisePause);
+// Режим глушіння, який передає завадовий сигнал
+void jammer()
+{
+  const char text[] = "xxxxxxxxxxxxxxxx"; // Текст для передачі шуму
+  for (int i = ((channels * 5) + 1); i < ((channels * 5) + 23); i++)
+  {
+    radio.setChannel(i);
+    radio.write(&text, sizeof(text));
+  }
+}
 
-  advancedSignalModulation();
-  int modPause = random(1000, 3000); 
-  Serial.print("Пауза перед наступним циклом модуляції: ");
-  Serial.print(modPause);
-  Serial.println(" мс");
-  
-  delay(modPause);
+// Головний цикл програми
+void loop()
+{
+  Serial.print("Сканування каналу: ");
+  Serial.println(channels + 1);
+  if (jamming)
+  {
+    Serial.print("ГЛУШЕННЯ КАНАЛУ ");
+    Serial.println(channels + 1);
+    radio.setPALevel(RF24_PA_HIGH); // Встановлюємо високу потужність сигналу
+    radio.setDataRate(RF24_2MBPS);  // Встановлюємо швидкість передачі 2 Мбіт/с
+  }
+  while (jamming)
+  {
+    jammer();                       // Активуємо глушіння
+  }
+
+  scanChannels();                   // Виконуємо сканування каналів
+  outputChannels();                 // Виводимо результат сканування
+
+  if (line++ > 12)
+  {
+    printChannels();                // Виводимо назви каналів
+    line = 0;
+  }
 }
